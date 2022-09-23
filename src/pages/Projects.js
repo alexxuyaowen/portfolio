3b6cb168352ece443f55c2c7403f3bbce7f6d520
Yaowen Xu <x@Macintosh.local>
2022-09-22 22:57:42 -0700

    +

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -1,18 +1,17 @@
 const Projects = () => (
   <div className='projects'>
+    <iframe
+      src='https://alexxuyaowen.github.io/gofive/'
+      title='GoFive'
+      width='700'
+      height='800'
+    />
+
     <img
       src='https://raw.githubusercontent.com/alexxuyaowen/maze/master/Maze/Demo/main.PNG'
       alt='Maze'
       onClick={() =>
         window.open('https://github.com/alexxuyaowen/maze', '_blank')
-      }
-    />
-
-    <img
-      src='https://raw.githubusercontent.com/alexxuyaowen/gofive/main/demo/demo.png'
-      alt='GoFive'
-      onClick={() =>
-        window.open('https://github.com/alexxuyaowen/gofive', '_blank')
       }
     />
 
